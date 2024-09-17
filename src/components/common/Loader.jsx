b7764021b8d2fcf411ddcf75b19a88b00421c1ab
sakshi pareek <sakshipareek505@gmail.com>
2024-09-17 17:07:15 +0530

    loader color

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -33,7 +33,7 @@ function Loader() {
   return (
     <section
       style={{ zIndex: "999999999" }}
-      className="bg_blue top-0 start-0 position-fixed h-100 w-100 d-flex flex-column justify-content-center align-items-center"
+      className="bg_sky top-0 start-0 position-fixed h-100 w-100 d-flex flex-column justify-content-center align-items-center"
     >
       <div className="d-flex justify-content-center align-items-center flex-column text-center">
       <div className="cs-loader-inner">
