53abab473bcb0178da02ff664a54edbc5284797f
Sakshi Pareek <sakshipareek505@gmail.com>
2025-05-16 12:15:34 +0530

    add height in hero section

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -55,7 +55,7 @@ export const Herosection = () => {
             <Container>
               <Row className="text-lg-start text-center align-items-center gap-lg-0 gap-5 py-5">
                 <Col lg={7}>
-                  <h2 className="fw-bold text-white fs_xxl">{slide.title}</h2>
+                  <h2 className="fw-bold text-white fs_xxl lh-1">{slide.title}</h2>
                   <p className="fw-normal text-white fs_md py-2 mb-4 text_justify">
                     {slide.description}
                   </p>
